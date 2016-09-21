1cd909d0870db8f96206af84dc2b38cfc49c8d95
universehuayuzhou <zhouhuayu@yahoo.com>
2016-09-21 13:23:23 -0700

    First commit

---
+++ server/src/api/SearchRestaurants.java
@@ -0,0 +1,61 @@
+package api;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+
+import db.DBConnection;
+import db.MongoDBConnection;
+import db.MySQLDBConnection;
+
+/**
+ * Servlet implementation class SearchRestaurants
+ */
+@WebServlet(description = "Search Restaurants near a location with latitude and longitude", urlPatterns = { "/restaurants" })
+public class SearchRestaurants extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static DBConnection connection = new MySQLDBConnection();
+
+	public SearchRestaurants() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// allow access only if session exists
+		if (!RpcParser.sessionValid(request)) {
+			response.setStatus(403);
+			return;
+		}
+		JSONArray array = null;
+		if (request.getParameterMap().containsKey("user_id")
+				&& request.getParameterMap().containsKey("lat")
+				&& request.getParameterMap().containsKey("lon")) {
+			String userId = request.getParameter("user_id");
+			double lat = Double.parseDouble(request.getParameter("lat"));
+			double lon = Double.parseDouble(request.getParameter("lon"));
+			array = connection.searchRestaurants(userId, lat, lon);
+		}
+		RpcParser.writeOutput(response, array);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
